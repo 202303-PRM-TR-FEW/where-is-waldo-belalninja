d4d164650ad0b15546403c384dc371b4f86f2239
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-10-12 19:24:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["P", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+];
 
 const whereIsWaldo = (matrix) => {
-  
-}
+  for (const [index, element] of matrix.entries()) {
+    debugger;
+    for (const [i, v] of element.entries()) {
+      debugger;
+      if (element[i + 1] == undefined || element[i - 1] == undefined) {
+        if (
+          element[i] !== element[i + 1] &&
+          element[i] !== element[i + 2] &&
+          element[i - 1] == undefined
+        ) {
+          return [index + 1, i + 1];
+        } else if (
+          element[i] !== element[i - 1] &&
+          element[i] !== element[i - 2] &&
+          element[i + 1] == undefined
+        ) {
+          return [index + 1, i + 1];
+        }
+      } else if (
+        element[i] !== element[i - 1] &&
+        element[i + 1] !== element[i]
+      ) {
+        return [index + 1, i + 1];
+      }
+    }
+  }
+};
+console.log(whereIsWaldo(example));
